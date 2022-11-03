10045e7be8d18a5af5e0db92b456cedb52d2f105
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-03 16:56:41 +0700

    CS002

---
+++ CS002/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double so_pi;
+        so_pi = 3.14;
+
+        double hai_pi;
+        hai_pi = 2 * so_pi;
+
+        Console.Title = "Vi du su dung Console";
+        Console.Clear();
+
+        Console.ForegroundColor = ConsoleColor.DarkMangenta;
+        Console.BackgroundColor = ConsoleColor.Black;
+
+        Console.WriteLine("Xin chao, chương trinh xuat du lieu console");
+
+        Console.ResetColor();
+
+        string hovaten;
+        Console.Write("Ho va ten cua ban:");
+        hovaten = Console.ReadLine();
+
+        Console.WriteLine("Xin chao {0}",hovaten);
+        float a, b;
+        string sinput;
+        Console.WriteLine("Nhap tham so a: ");
+        sinput = Console.ReadLine();
+        a = float.Parse(sinput);
+        
+        Console.WriteLine("Nhap tham so b:" );
+        sinput = Console.ReadLine();
+        b = Console.ToSingle(sinput);
+        
+        Console.WriteLine("So a = {0} So b = {1}",a,b);
+               
+        Console.WriteLine(ten);
+           
+
+    }
+}
