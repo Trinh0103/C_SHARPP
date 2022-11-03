56a15990436aa8aa71b0365f8dbe650920ce5a35
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-03 16:59:33 +0700

    CS004

---
+++ CS004/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool kq;
+        // == > < >= <= !=
+        // && || !
+        //isOnline = true;
+        //isOnline = false;
+        /*int a = 5;
+        int b = 6;
+
+        kq = a == b;
+
+        Console.WriteLine("a == b ==> {0}", kq);
+        Console.WriteLine($"a != b ==> {a != b}");
+        Console.WriteLine($"a > b ==> {a > b}");
+        Console.WriteLine($"a >= b ==> {a >= b}");
+        Console.WriteLine($"a < b ==> {a < b}");
+        Console.WriteLine($"a <= b ==> {a <= b}");
+        */
+
+        bool a = false;
+        bool b = true;
+
+        kq = a || b;
+
+        Console.WriteLine(kq);
+    }
+}
