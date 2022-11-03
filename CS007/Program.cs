095bd4278028d2ca373ae04b735d92d5c893cbda
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-03 17:41:29 +0700

    CS007

---
+++ CS007/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //for
+        for (int i = 8;i <=10;i++)
+        {
+            Console.WriteLine("So i = " + i);
+        }
+
+        //while
+        int j = 8;
+        while (j <= 10)
+        {
+            Console.WriteLine("So j = " + j);
+            j++;
+        }
+        
+        //do..while
+        int a = 10;
+        do
+        {
+            Console.WriteLine("So a = " + a);
+            a += 2;
+        }
+        while (a <= 20);
+
+        //continue
+        for (int b = 10; b <= 20; b++)
+        {
+            if (b % 3 != 0)
+                continue;
+            Console.WriteLine("So b = " + b);
+        }
+
+    }
+}
