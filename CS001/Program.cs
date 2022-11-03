f323c652915af6a5236c949981dd1fe3adfb0870
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-03 16:54:20 +0700

    CS001

---
+++ CS001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS001
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
