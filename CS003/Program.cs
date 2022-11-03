1c993871d879e38bc7eb2ea486255f2f9bf5b92f
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-03 16:58:04 +0700

    CS003

---
+++ CS003/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CS003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* + - * / %
+            ()
+            float a = 5;
+            int b = 4;
+
+            /*Console.WriteLine("a + b = {0}", a + b);
+            Console.WriteLine("a - b = {0}", a - b);
+            Console.WriteLine("a * b = {0}", a * b);
+            Console.WriteLine("a / b = {0}", a / b);
+            Console.WriteLine("a % b = {0}", a % b);
+            
+            float kq = 6 / (2 + 3) * 2;
+
+            Console.WriteLine(kq);
+            */
+
+            // GAN
+            // = += -= /= %=
+            int x = 10;
+            //x = x + 2;
+            //x += 2;
+            x -= 7;
+
+            Console.WriteLine(x);
+        }
+    }
+}
