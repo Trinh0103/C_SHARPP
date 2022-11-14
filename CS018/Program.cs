982a67a12f29578b5785c5ac29d25ddaaa5f4063
Trinh0103 <117304137+Trinh0103@users.noreply.github.com>
2022-11-14 22:04:56 +0700

    CS018

---
+++ CS018/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace CS018
+{
+    class Abc 
+    {
+        public void Xinchao() => Console.WriteLine("Xin chao C#");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // nulln nullable
+            // null
+
+            //Abc a = new Abc();
+
+            //if (a != null)
+            //    a.Xinchao();
+            //a?.Xinchao();
+
+            int? age;
+            age = null;
+
+            age = 100;
+
+            //if (age.HasValue)
+            if (age != null)
+            {
+                //int _age = age.Value;
+                int _age = (int)age;
+                Console.WriteLine(_age);
+            }
+
+        }
+    }
+}
